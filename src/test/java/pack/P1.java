44f8948ff56ca0c5f1e9f378a3614e77a87f6e95
Dell <Dell@DESKTOP-DIGQVLB>
2023-11-15 11:28:15 +0530

    Demo class pushed

---
+++ src/test/java/pack/P1.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class P1 {
+public static void main(String[] args) {
+	
+	System.out.println("Bebo Nehan");
+	
+	System.out.println("Good Morning");
+}
+}
---
+++ src/test/java/pack/P2.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class P2 {
+public static void main(String[] args) {
+	
+	System.out.println("I L U");
+}
+}
